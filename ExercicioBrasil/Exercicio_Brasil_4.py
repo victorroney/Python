0aa6a6182df57a29045748722039ddf9f3ffb732
Roney Victor Santos <54716734+victorroney@users.noreply.github.com>
2021-02-17 01:51:14 -0300

    Add files via upload

---
+++ ExercicioBrasil/Exercicio_Brasil_4.py
@@ -0,0 +1,5 @@
+bim1=float(input("Digite as notas do primeiro bimestre"))
+bim2=float(input("Digite as notas do segundo bimestre"))
+bim3=float(input("Digite as notas do terceiro bimestre"))
+bim4=float(input("Digite as notas do quarto bimestre"))
+
